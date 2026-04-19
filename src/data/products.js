ecb9b3c798abf5a3f8f525aeba0a1acf5cc8a414
slucena28 <113914867+slucena28@users.noreply.github.com>
2026-04-19 15:33:09 -0300

    Add products data to products.js

---
+++ src/data/products.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: "Brigadeiro",
+    description: "Brigadeiro gourmet delicioso",
+    price: 5
+  },
+  {
+    id: 2,
+    name: "Bolo de Chocolate",
+    description: "Fofinho com cobertura cremosa",
+    price: 25
+  },
+  {
+    id: 3,
+    name: "Cupcake",
+    description: "Colorido e irresistível",
+    price: 8
+  }
+];
